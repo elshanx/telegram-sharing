73a00cdffad2a9030702081ec5b237d246045ae8
elshanx <elshanxakbarov@gmail.com>
2021-01-05 18:20:38 +0400

    ready component for future use

---
+++ frontend/src/Components/Sponsors/index.jsx
@@ -0,0 +1,26 @@
+import './index.scss';
+
+export const Sponsors = () => {
+  return (
+    <div className='blog-post'>
+      <div className='blog-posts__img'>
+        <img src='https://picsum.photos/200/300' alt='' />
+      </div>
+      <div className='blog-post__info'>
+        <div className='blog-post__date'>
+          <span>Sunday</span>
+          <span>october 27 2021</span>
+        </div>
+        <h1 className='blog-post__title'>lorem ipsum</h1>
+        <p className='blog-post__tex'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium enim
+          ut quisquam quasi eligendi suscipit porro corporis perspiciatis dolorum
+          nobis sed in nulla maxime nemo quo, impedit ab deserunt similique.
+        </p>
+        <a href='#' className='blog-post__cta'>
+          read more
+        </a>
+      </div>
+    </div>
+  );
+};
