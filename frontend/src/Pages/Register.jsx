11ed4a97711008bf26d1cca026d20e7743c1875b
elshanx <elshanxakbarov@gmail.com>
2021-01-25 04:45:51 +0400

    completed styling register page

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -6,8 +6,8 @@ import {
   StyledButton,
   StyledInput,
   StyledHeading,
-  StyledLink,
   StyledForm,
+  StyledLink as StyledToLogin,
 } from '../Styles/Styled';
 import backgroundVideo from '../Styles/assets/charles-parker.mp4';
 
@@ -15,26 +15,24 @@ import { register } from '../API';
 import { useAuth } from '../Providers/AuthContext';
 
 const Register = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
   const { setCurrentUser } = useAuth();
 
-  const formData = {
-    firstName,
-    lastName,
-    email,
-    password,
-    confirmPassword,
+  const onInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const registerUser = async (e) => {
     e.preventDefault();
     try {
       const data = await register(formData);
-      setCurrentUser({ firstName });
+      // setCurrentUser({ firstName });
       console.log(data);
     } catch (error) {
       console.log(error);
@@ -50,56 +48,55 @@ const Register = () => {
         </StyledLink>
 
         <StyledVideoContainer>
-          <video
-            width='600'
-            height='600'
-            className='video'
-            // muted autoPlay loop
-          >
+          <video muted autoPlay loop>
             <source type='video/mp4' src={backgroundVideo} />
           </video>
         </StyledVideoContainer>
 
         <StyledFormContainer>
-          <StyledForm className='register-form' onSubmit={registerUser}>
+          <StyledForm onSubmit={registerUser}>
             <StyledHeading>Sign Up</StyledHeading>
             <StyledInput
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              name='firstName'
+              onChange={onInputChange}
+              value={formData.firstName}
               type='text'
-              placeholder='Your name'
+              placeholder='First name'
             />
             <StyledInput
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              name='lastName'
+              onChange={onInputChange}
+              value={formData.lastName}
               type='text'
-              placeholder='Your name'
+              placeholder='Last name'
             />
             <StyledInput
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              name='email'
+              onChange={onInputChange}
+              value={formData.email}
               type='text'
               placeholder='Your email'
             />
             <StyledInput
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name='password'
+              onChange={onInputChange}
+              value={formData.password}
               type='password'
               placeholder='Create password'
             />
             <StyledInput
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
+              name='password2'
+              onChange={onInputChange}
+              value={formData.password2}
               type='password'
               placeholder='Confirm password'
             />
             <StyledButton auth type='submit'>
               Register
             </StyledButton>
-            <StyledLink size='small'>
-              Already have an account?
-              <Link to='/login'> Log in</Link>
-            </StyledLink>
+            <StyledToLogin size='small'>
+              Already have an account? <Link to='/login'>Log in</Link>
+            </StyledToLogin>
           </StyledForm>
         </StyledFormContainer>
       </StyledRegisterContainer>
@@ -114,7 +111,7 @@ const StyledSection = styled.section`
   justify-content: center;
   align-items: center;
 
-  /* background: linear-gradient(
+  background: linear-gradient(
     -45deg,
     #3f51b1 0%,
     #5a55ae 13%,
@@ -125,104 +122,60 @@ const StyledSection = styled.section`
     #f18271 75%,
     #f3a469 87%,
     #f7c987 100%
-  ); */
+  );
 `;
 
 const StyledRegisterContainer = styled.div`
+  border: none;
+  outline: none;
   width: 85%;
   margin: 0 auto;
-
   display: flex;
   justify-content: center;
   align-items: center;
-
-  border-radius: 10px;
+  border-radius: 5px;
   overflow: hidden;
   width: max-content;
   height: 60%;
   box-shadow: 0 6.7px 5.3px rgba(0, 0, 0, 0.028),
     0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 100px 80px rgba(0, 0, 0, 0.07);
-
-  & .form-title {
-    margin-bottom: 1rem;
-  }
 `;
 
 const StyledVideoContainer = styled.div`
-  &.video-container {
-    overflow: hidden;
-  }
+  height: 600px;
 
-  & .video {
-    width: 300px;
+  video {
     height: 100%;
+    width: 300px;
     object-fit: fill;
   }
 `;
 
 const StyledFormContainer = styled.div`
   padding: 2rem;
-  background: #e4e2e5;
+  background: rgb(40, 43, 44);
   width: 500px;
-
-  & .register-form {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    & > input {
-      width: 250px;
-      padding: 5px 10px;
-      margin: 5px;
-      border: 1px solid transparent;
-      border-radius: 5px;
-      font-size: 16px;
-      line-height: 1.7;
-      transition: border 0.2s ease-in;
-
-      &:focus {
-        border: 1px solid $primary;
-        outline: none;
-      }
-    }
-
-    & .reg-btn {
-      padding: 0.3rem 0;
-      width: 250px;
-      margin: 1rem;
-      border: none;
-      border-radius: 4px;
-      background: $primary;
-      font-weight: 500;
-      color: #e4e2e5;
-      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
-        0 6px 6px rgba(0, 0, 0, 0.23);
-      transition: all 0.2s ease-in;
-
-      &:hover {
-        color: $primary;
-        background: #e4e2e5;
-      }
-
-      &:active {
-        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.19);
-      }
-    }
-  }
+  height: 600px;
 `;
 
-const StyledBackBtn = styled.button`
-  font-weight: bold;
-  position: absolute;
-  top: 20px;
-  left: 10%;
-  transition: color 0.2s ease-in;
+const StyledLink = styled.p`
+  color: ${({ theme }) => theme.primaryLight};
+  font-weight: 500;
+  font-size: ${({ size }) => (size === 'small' ? '0.6rem' : '1rem')};
+  opacity: ${({ size }) => (size === 'small' ? 0.6 : 1)};
 
-  &:hover {
-    text-decoration: none;
-    color: red;
+  position: absolute;
+  top: 2rem;
+  width: 85%;
+
+  & a {
+    background: rgba(51, 51, 51, 0.397);
+    padding: 0.5rem;
+    border-radius: 3px;
+
+    &:hover {
+      background: rgba(51, 51, 51, 0.137);
+    }
   }
 `;
 
