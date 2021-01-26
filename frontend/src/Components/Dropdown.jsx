1fc26b454750af3e0033290a690d6ce5c1759a8e
elshanx <elshanxakbarov@gmail.com>
2021-01-26 16:44:36 +0400

    toggling dropdown

--- frontend/src/Components/Dropdown.jsx
+++ frontend/src/Components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Dropdown = ({ title }) => {
   const [open, setOpen] = useState(false);
@@ -7,66 +8,85 @@ const Dropdown = ({ title }) => {
     <div className='relative inline-block text-left'>
       <div>
         <button
+          onClick={() => setOpen(!open)}
           type='button'
           className='flex justify-center w-full rounded-sm border-none shadow-sm py-1 px-4 bg-altdark transition-all hover:bg-mainhover font-medium text-white focus:outline-none outline-none'
-          id='options-menu'
           aria-haspopup='true'
           aria-expanded='true'
         >
           <h2>{title}</h2>
 
-          <svg
-            className='-mr-3 ml-2 h-6 w-5'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 20 20'
-            fill='currentColor'
-            aria-hidden='true'
-          >
-            <path
-              fillRule='evenodd'
-              d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-              clipRule='evenodd'
-            />
-          </svg>
+          {!open ? (
+            <svg
+              className='-mr-3 ml-2 h-6 w-5'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              aria-hidden='true'
+            >
+              <path
+                fillRule='evenodd'
+                d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                clipRule='evenodd'
+              />
+            </svg>
+          ) : (
+            <svg
+              className='-mr-3 ml-2 h-6 w-5'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z'
+                clip-rule='evenodd'
+              />
+            </svg>
+          )}
         </button>
       </div>
-      <div className='hidden origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
+      <div
+        className={
+          open
+            ? 'origin-top-right absolute right-0 mt-2 w-40 rounded shadow-lg font-medium bg-white ring-1 ring-black ring-opacity-5 transition-all duration-300'
+            : 'transition duration-300 hidden'
+        }
+      >
         <div
           className='py-1'
           role='menu'
           aria-orientation='vertical'
           aria-labelledby='options-menu'
         >
-          <a
-            href='#'
-            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+          <Link
+            to='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition duration-300'
             role='menuitem'
           >
-            Account settings
-          </a>
-          <a
+            Some Group
+          </Link>
+          <Link
             href='#'
-            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition duration-300'
             role='menuitem'
           >
-            Support
-          </a>
-          <a
-            href='#'
-            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+            Some Channel
+          </Link>
+          <Link
+            to='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition duration-300'
             role='menuitem'
           >
-            License
-          </a>
-          <form method='POST' action='#'>
-            <button
-              type='submit'
-              className='block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900'
-              role='menuitem'
-            >
-              Sign out
-            </button>
-          </form>
+            Some
+          </Link>
+          <Link
+            to='#'
+            className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition duration-300'
+            role='menuitem'
+          >
+            Go somewhere
+          </Link>
         </div>
       </div>
     </div>
