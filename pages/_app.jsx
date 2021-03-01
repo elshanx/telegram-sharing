94fa60253e75599c475cb7bbfa10eab4a72cf83f
elshanx <elshanxakbarov@gmail.com>
2021-03-01 22:39:07 +0400

    removed basewebui - added sass

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,7 @@
-import React from 'react';
-import App from 'next/app';
-import { Provider as StyletronProvider } from 'styletron-react';
-import { LightTheme, BaseProvider } from 'baseui';
-import { styletron } from '../styletron';
+import '../styles/global.scss';
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <StyletronProvider value={styletron}>
-        <BaseProvider theme={LightTheme}>
-          <Component {...pageProps} />
-        </BaseProvider>
-      </StyletronProvider>
-    );
-  }
-}
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
