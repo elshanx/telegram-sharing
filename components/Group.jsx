e3a42c04dff12c7fc45f62b7d63f4ee41c6d9f31
elshanx <elshanxakbarov@gmail.com>
2021-03-02 22:57:36 +0400

    added group skeleton

--- components/Group.jsx
+++ components/Group.jsx
@@ -1,14 +1,26 @@
-const Group = () => {
+import { card, cardContainer, flex } from 'styles/group.module.scss';
+
+import Skeleton from 'components/Skeleton';
+
+const Group = ({ data }) => {
+  if (!data)
+    return (
+      <div className={flex}>
+        <Skeleton />
+      </div>
+    );
+
   return (
-    <div className='flex justify-around items-center shadow dark:bg-light text-altdark font-medium rounded-sm overflow-hidden relative p-8 my-4 transition transform hover:scale-105'>
-      <div className='mr-8 rounded-full overflow-hidden border-r-4 border-yellow-300 shadow-lg'>
-        <img loading='lazy' src='https://picsum.photos/100/100' alt='' />
-      </div>
-      <div>
-        <h3 className='w-48'>Lorem ipsum dolor sit amet</h3>
-        <span className='text-altdark opacity-70'>participants count</span>
-        <span className='text-altdark opacity-70'>date created</span>
-      </div>
+    <div className={cardContainer}>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
+      <div className={card}></div>
     </div>
   );
 };
