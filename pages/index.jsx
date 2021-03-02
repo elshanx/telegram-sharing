d9adeb7525d4a8d22406f4e4a0e54407372d35e6
elshanx <elshanxakbarov@gmail.com>
2021-03-02 23:01:46 +0400

    refactored index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,11 @@
-import Navbar from '../components/Navbar';
-import Header from '../components/Header';
-import GroupList from '../components/GroupList';
-import Footer from '../components/Footer';
+import GroupList from 'components/GroupList';
+import Navbar from 'components/Navbar';
 
 const Home = () => (
-  <div className='w-full dark:bg-primary bg-gray-400 dark:text-light'>
-    <div className='container m-auto'>
-      <Navbar />
-      <Header />
-      <GroupList />
-      <Footer />
-    </div>
-  </div>
+  <>
+    <Navbar />
+    <GroupList />
+  </>
 );
 
 export default Home;
