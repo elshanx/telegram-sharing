8d274f801a84bc18ec06217bab6a3d832cd00d6a
elshanx <elshanxakbarov@gmail.com>
2021-03-02 22:56:33 +0400

    added sidebar skeleton and removed deps

--- components/Skeleton.jsx
+++ components/Skeleton.jsx
@@ -1,14 +1,17 @@
 import MUISkeleton from '@material-ui/lab/Skeleton';
-import { flex } from 'styles/Group.module.scss';
-import { nanoid } from 'nanoid';
 
-const Skeleton = ({ n }) => {
-  return [...Array(n)].map((_) => (
-    <div className={flex} key={nanoid()}>
-      <MUISkeleton width={250} height={260} />
-      <MUISkeleton width={250} height={260} />
-      <MUISkeleton width={250} height={260} />
-    </div>
+const Skeleton = ({ side }) => {
+  if (side) {
+    return [...Array(3)].map((_) => (
+      <MUISkeleton animation='wave' width={300} height={260} />
+    ));
+  }
+  return [...Array(3)].map((_) => (
+    <>
+      <MUISkeleton animation='wave' width={300} height={260} />
+      <MUISkeleton animation='wave' width={300} height={260} />
+      <MUISkeleton animation='wave' width={300} height={260} />
+    </>
   ));
 };
 
