0dd26b4de09aa1ae4607343920b9164074874ae6
elshanx <elshanxakbarov@gmail.com>
2021-03-03 19:48:19 +0400

    page transitions with motion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,21 @@
 import '../styles/global.scss';
 
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import { motion } from 'framer-motion';
+
+const MyApp = ({ Component, pageProps, router }) => {
+  return (
+    <motion.div
+      key={router.route}
+      initial='pageInitial'
+      animate='pageAnimate'
+      variants={{
+        pageInitial: { opacity: 0 },
+        pageAnimate: { opacity: 1 },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 };
 
 export default MyApp;
